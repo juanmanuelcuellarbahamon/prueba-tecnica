91718ba3f4ad414f3592ed2d1e9a3febf4847e58
JUAN MANUEL CUELLAR BAHAMON <juanmanuelcuellarbahamon123@gmail.com>
2025-05-16 19:37:44 -0500

    a

--- backend/src/auth/entity/user.entity.ts
+++ backend/src/auth/entity/user.entity.ts
@@ -47,13 +47,13 @@ export class User {
   })
   role: UserRole;
 
-  @Column({ type: 'decimal', precision: 100000000, scale: 2, default: 0 })
+  @Column({ type: 'decimal', precision: 65, scale: 2, default: 0 })
   balanceCOP: number;
 
-  @Column({ type: 'decimal', precision: 100000000, scale: 2, default: 0 })
+  @Column({ type: 'decimal', precision: 65, scale: 2, default: 0 })
   balanceUSD: number;
 
-  @Column({ type: 'decimal', precision: 100000000, scale: 2, default: 0 })
+  @Column({ type: 'decimal', precision: 65, scale: 2, default: 0 })
   balanceEUR: number;
 
   @Column({ type: 'text', nullable: true })
